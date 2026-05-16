fea5d17e374a01154d689fb075f71e86f2d501ab
Christoph Drechsel <drechselchris@gmail.com>
2026-05-16 18:29:29 +0200

    Add portfolio weight tracking and visualization

--- src/portfolio.py
+++ src/portfolio.py
@@ -30,6 +30,7 @@ def weighted_portfolio_returns(
 def monthly_rebalance_with_drift(
     returns: pd.DataFrame,
     target_weights: pd.Series,
+    return_weights: bool = False
 ) -> pd.Series:
     """
     Simulate a monthly rebalanced portfolio with intra-month weight drift.
@@ -38,6 +39,8 @@ def monthly_rebalance_with_drift(
     target_weights = target_weights.reindex(returns.columns).fillna(0.0)
 
     portfolio_returns = []
+    weights_history = []
+
     current_weights = target_weights.copy()
 
     previous_month = None
@@ -45,18 +48,30 @@ def monthly_rebalance_with_drift(
     for date, daily_returns in returns.iterrows():
         current_month = (date.year, date.month)
 
+        # once new month, set weights back to target weights
         if current_month != previous_month:
             current_weights = target_weights.copy()
             previous_month = current_month
-
+            
+        weights_history.append(
+            {"date": date, **current_weights.to_dict()}
+        )
         portfolio_return = (current_weights * daily_returns).sum()
         portfolio_returns.append((date, portfolio_return))
 
         current_weights = current_weights * (1 + daily_returns)
         current_weights = current_weights / current_weights.sum()
 
-    return pd.Series(
+    portfolio_returns = pd.Series(
         data=[r for _, r in portfolio_returns],
         index=[d for d, _ in portfolio_returns],
         name="portfolio_return",
     )
+
+    weights_history = pd.DataFrame(weights_history)
+    weights_history = weights_history.set_index("date")
+
+    if return_weights:
+        return portfolio_returns, weights_history
+
+    return portfolio_returns
